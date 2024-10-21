dc86a914b263aba0bdf703fa155480df59e1d48a
Nalina <kv.nalinapramod@gmail.com>
2024-10-21 16:33:28 -0400

    Created core URDF

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,27 +1,27 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 import xacro
-
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    # Declare the use_sim_time argument
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    # Process the URDF file
+    # Set the path to the xacro file and the inertial macros file
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     
-    # Create a robot_state_publisher node
+    # Process the xacro file
+    robot_description_config = xacro.process_file(xacro_file)
+
+    # Set the robot description parameters
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+
+    # Define the robot_state_publisher node
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -29,13 +29,13 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
-    # Launch!
+    # Return the launch description with the necessary nodes
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
+            description='Use sim time if true'
+        ),
         node_robot_state_publisher
     ])
+
